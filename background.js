2f3cc056d88780822b995b107d48dffdae76b263
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-28 15:39:00 +0200

    inital

---
+++ background.js
@@ -0,0 +1,21 @@
+
+const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+
+browser.menus.create({   
+	id: extname,
+	title: "Prepend to Clipboard",
+	documentUrlPatterns: [ "<all_urls>" ],
+	contexts: ["page", "link", "image", "editable", "selection"],
+	onclick: async function(clickData,tab) {
+		let selected = clickData.linkUrl || clickData.selectionText || clickData.srcUrl || '';
+		if(temporary){ console.log('selected', selected); }
+		let content = await navigator.clipboard.readText();
+		if(temporary){ console.log('content', content); }
+		content = selected + "\n" + content;
+		await navigator.clipboard.writeText(content);
+		if(temporary){ console.log('content', content); }
+	}
+});
+
