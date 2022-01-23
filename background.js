165ac6f2e7c1381edd5f54bcd6020a46830363ad
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-01-23 13:45:35 +0100

    chore: rename addon feat: add context entry to append selection'

--- background.js
+++ background.js
@@ -3,9 +3,9 @@ const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
-browser.menus.create({   
+browser.menus.create({
 	id: extname,
-	title: "Prepend to Clipboard",
+	title: "Prepend",
 	documentUrlPatterns: [ "<all_urls>" ],
 	contexts: ["page", "link", "image", "editable", "selection"],
 	onclick: async function(clickData,tab) {
@@ -19,3 +19,18 @@ browser.menus.create({
 	}
 });
 
+browser.menus.create({
+	id: extname,
+	title: "Append",
+	documentUrlPatterns: [ "<all_urls>" ],
+	contexts: ["page", "link", "image", "editable", "selection"],
+	onclick: async function(clickData,tab) {
+		let selected = clickData.linkUrl || clickData.selectionText || clickData.srcUrl || '';
+		if(temporary){ console.log('selected', selected); }
+		let content = await navigator.clipboard.readText();
+		if(temporary){ console.log('content', content); }
+		content = content + "\n" + selected + "\n";
+		await navigator.clipboard.writeText(content);
+		if(temporary){ console.log('content', content); }
+	}
+});
